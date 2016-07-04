79f7977991d6baaeefa7a48a7090e0bea8fd6469
roger <roger@idsoption.com>
2016-07-04 10:27:47 +0900

    기본적인 posts 목록 템플릿과 정적 데이터 추가

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,21 @@
+/**
+ * Created by roger on 2016. 7. 4..
+ */
+const postsData = [
+  {
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  },
+  {
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  },
+  {
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  }
+];
+
+Template.postsList.helpers({
+  posts : postsData
+});
