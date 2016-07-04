80b549f04ea69396179338dff2026b5ad9d4d013
roger <roger@idsoption.com>
2016-07-04 16:27:31 +0900

    컬렉션을 'postsList' 템플릿과 연동했다

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,21 +1,8 @@
 /**
  * Created by roger on 2016. 7. 4..
  */
-const postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  }
-];
-
 Template.postsList.helpers({
-  posts : postsData
+  posts : () => {
+    return Posts.find();
+  }
 });
