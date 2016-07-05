44cbc6197ee024bf5b3477574e865732605a6668
roger <roger@idsoption.com>
2016-07-05 17:18:27 +0900

    Meteor method를 사용하여 post 등록

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -2,15 +2,20 @@
  * Created by roger on 2016. 7. 5..
  */
 Template.postSubmit.events({
-  'submit form' : (e) => {
+  'submit form': (e) => {
     e.preventDefault();
 
     const post = {
-      url : $(e.target).find('[name=url]').val(),
-      title : $(e.target).find('[name=title]').val()
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
     };
 
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+
+    Meteor.call('postInsert', post, (error, result) => {
+      if (error)
+        return alert(error.reason);
+
+      Router.go('postPage', result._id);
+    });
   }
 });
