5da9f45b3b9852d5d11a72766c4b2278fc8d014e
roger <roger@idsoption.com>
2016-07-13 16:25:11 +0900

    일단 하라는데로 하긴했는데.. 안됨 ㅠ
    우선 커밋 후 이전 커밋으로 돌아가서 다시 작업 예정

---
+++ packages/errors/errors_test.js
@@ -0,0 +1,31 @@
+/**
+ * Created by roger on 2016. 7. 13..
+ */
+Tinytest.add("Errors collection works", function (test) {
+  test.equal(Errors.collection.find({}).count(), 0);
+
+  Errors.throw("A new error!");
+  test.equal(Errors.collection.find({}).count(), 1);
+
+  Errors.collection.remove({});
+});
+
+Tinytest.addAsync("Errors template works", function (test, done) {
+  Errors.throw('A new error!');
+  test.equal(Errors.collection.find({seen: false}).count(), 1);
+
+  OnscreenDiv(Spark.render(function () {
+    return Template.meteorErrors();
+  }));
+
+  Meteor.setTimeout(function () {
+    test.equal(Errors.collection.find({seen : false}).count(), 0);
+
+    test.equal(Errors.collection.find({}).count(), 1);
+
+    Errors.clearSeen();
+
+    test.equal(Errors.collection.find({seen: true}).count(), 0);
+    done();
+  }, 500);
+});
--- packages/errors/package.js
+++ packages/errors/package.js
@@ -5,11 +5,18 @@ Package.describe({
   summary: "A pattern to display application errors to the user"
 });
 
-Package.on_use(function (api, where) {
+Package.onUse(function (api, where) {
   api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
 
-  api.add_files(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+  api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
 
   if (api.export)
     api.export('Errors');
+});
+
+Package.onTest(function (api) {
+  api.use('redbyzan:errors', 'client');
+  api.use(['tinytest', 'test-helpers'], 'client');
+
+  api.addFiles('errors_test.js', 'client');
 });
