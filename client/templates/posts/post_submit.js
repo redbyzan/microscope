866c652ea3be1e79748cb665a93025b756d29ec7
roger <roger@idsoption.com>
2016-07-13 13:51:50 +0900

    post 등록 시 콘텐츠 유효성 검사

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,6 +1,21 @@
 /**
  * Created by roger on 2016. 7. 5..
  */
+Template.postSubmit.created = function () {
+  Session.set('postSubmitErrors', {});
+}
+
+Template.postSubmit.helpers({
+
+  errorMessage: function (field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+
+});
+
 Template.postSubmit.events({
   'submit form': (e) => {
     e.preventDefault();
@@ -9,6 +24,10 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
+
+    const errors = validatePost(post);
+    if (errors.title || errors.url)
+      return Session.set('postSubmitErrors', errors);
 
     Meteor.call('postInsert', post, (error, result) => {
       if (error)
